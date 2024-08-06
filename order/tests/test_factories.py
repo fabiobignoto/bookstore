c7a486f068542b6a1b3ab67517f4a126b104e406
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-08-06 15:33:33 -0300

    Order factory test adjusted

--- order/tests/test_factories.py
+++ order/tests/test_factories.py
@@ -13,10 +13,38 @@ class OrderFactoryTest(TestCase):
             order.product, msg="Order's Product is equivalent to False.")
 
     def test_order_factory_add_products(self):
-        product_one = ProductFactory()
-        product_two = ProductFactory()
-        order = OrderFactory(product_one, product_two)
-        ...
+        product_one = ProductFactory(title='First Product')
+        product_two = ProductFactory(title='Second Product')
+        product_three = ProductFactory(title='Third Product')
+
+        order = OrderFactory(product=[product_one, product_three])
+        product_list = list()
+        for product in order.product.all():
+            product_list.append(product.title)
+
+        self.assertIn(
+            product_one.title,
+            product_list,
+            msg='Expected product title not found in the order.'
+        )
+
+        self.assertIn(
+            product_three.title,
+            product_list,
+            msg='Expected product title not found in the order.'
+        )
+
+        self.assertNotIn(
+            product_two.title,
+            product_list,
+            msg='Unexpected product title found in the order.'
+        )
+
+        self.assertEqual(
+            2,
+            len(product_list),
+            msg='Wrong number of products found in the order.'
+        )
 
 
 class UserFactoryTest(TestCase):
