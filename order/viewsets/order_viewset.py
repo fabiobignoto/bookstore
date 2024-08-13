0354d243efcbb76a4ec5f3ebf1f827900d89793b
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-08-13 10:39:12 -0300

    Token authentication added to order_viewset.

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -3,7 +3,16 @@ from rest_framework.viewsets import ModelViewSet
 from order.models.order import Order
 from order.serializers.order_serializer import OrderSerializer
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 class OrderViewSet(ModelViewSet):
+    authentication_classes = [
+        SessionAuthentication,
+        BasicAuthentication,
+        TokenAuthentication
+    ]
+    permission_classes = [IsAuthenticated]
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+    queryset = Order.objects.all().order_by('-id')
